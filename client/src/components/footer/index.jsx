59022e8ad9c164bbf76f66bf0e84e2a118e6ef30
David Davies <David_Davies@mail.com>
2022-04-18 17:09:33 +0100

    Started adding the footer component

---
+++ client/src/components/footer/index.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+
+
+const FooterContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 6rem;
+  padding-top: 2rem;
+  align-items: center;
+  background-color: rgba(7, 27, 69, 0.9);
+  border-top: 1px solid #ffdd19;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
+
+const FooterLeft = styled.div`
+  display: flex;
+  margin-right: 4rem;
+`;
+const FooterMiddle = styled.div`
+  display: flex;
+  flex: 2;
+  height: 100%;
+  justify-content: center;
+`;
+const FooterRight = styled.div`
+  display: flex;
+  margin-right: 4rem;
+`;
+
+export function Footer(props) {
+  
+  return (
+    <FooterContainer>
+      
+      <FooterLeft>
+              </FooterLeft>
+      <FooterMiddle>
+
+      </FooterMiddle>
+      <FooterRight>
+        
+      </FooterRight>
+    </FooterContainer>
+  );
+}
