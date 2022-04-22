46ac9bd4703c0d3ec7ce3dfea811f204aa063493
David Davies <David_Davies@mail.com>
2022-04-22 23:20:27 +0100

    added pictures and content for projects page

---
+++ client/src/content.jsx
@@ -0,0 +1,58 @@
+const content = [
+  {
+    id: 1,
+    title: "Teachers Toolbox",
+    body: "Current Project a toolbox to assist teachers in managing and teaching their class & children.",
+    image: "ttoolbox.png",
+    githubLink: "https://github.com/DaveyDavey1901/Teachers_ToolBox",
+    figma:
+      "https://www.figma.com/file/9MsuHDP4wP7jdP83ElmwKw/Planning-The-Project",
+  },
+  {
+    id: 2,
+    title: "Team Task Manager",
+    body: "A great little app to help manage your teams. To test use ( Username: testmanager / Password = password1 )",
+    image: "ttmanager.png",
+    liveLink: "https://task-manager-dd.herokuapp.com/login",
+    githubLink: "https://github.com/DaveyDavey1901/team-task-manager",
+    figma:
+      "https://www.figma.com/file/oMKGmhjqmJd6OpjFZrGi7i/Team_Task_Manager?node-id=0%3A1",
+  },
+  {
+    id: 3,
+    title: "Safe Home",
+    body: "An App to help you find a safe home for you and family.",
+    image: "safehome.png",
+    liveLink: "https://daveydavey1901.github.io/Safe-Home/",
+    githubLink: "https://daveydavey1901.github.io/Safe-Home/",
+  },
+  {
+    id: 4,
+    title: "Timed Code Quiz",
+    body: "Test your knowledge on this timed code quiz.",
+    image: "codeQuiz.png",
+    liveLink: "https://daveydavey1901.github.io/timedCodeQuiz/",
+    githubLink: "https://daveydavey1901.github.io/timedCodeQuiz/",
+  },
+  {
+    id: 5,
+    title: "Password Generator",
+    body: "Need a strong password make one with this!",
+    image: "pwdgenerator.png",
+    liveLink: "https://daveydavey1901.github.io/randPassGenerator/",
+    githubLink: "https://daveydavey1901.github.io/randPassGenerator/",
+  },
+  {
+    id: 6,
+    title: "Weather Dashboard",
+    body: "A Dashboard to display the current and 5 day forcast",
+    image: "weatherapi.png",
+    liveLink: "https://daveydavey1901.github.io/WeatherDB/",
+    githubLink: "https://daveydavey1901.github.io/WeatherDB/",
+  },
+];
+
+export default content
+
+
+      
