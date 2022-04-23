120291eefaaf164b8306dbf107c1447879432042
David Davies <David_Davies@mail.com>
2022-04-23 14:55:19 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import { ThemeSize } from "./components/Responsive/index";
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
 
 // pages
 import { Home } from "./Pages/Homepage";
@@ -19,16 +19,16 @@ function App() {
   return (
     <ThemeProvider theme={ThemeSize}>
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      <Footer />
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
